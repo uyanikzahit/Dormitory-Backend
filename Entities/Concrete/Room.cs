4072de3e7e637ce6608ceec564468b6463e0cfa6
uyanikmehmetzahit <uyanikmehmetzahit@gmail.com>
2023-12-05 23:17:11 +0300

    Update

---
+++ Entities/Concrete/Room.cs
@@ -0,0 +1,18 @@
+﻿using Core.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+    public class Room : IEntity
+    {
+        public int RoomId {  get; set; }
+
+        public int RoomNumber { get; set; }
+
+
+    }
+}
