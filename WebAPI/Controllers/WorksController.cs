81373618f373f80f2f864071400ab4832dce862f
uyanikmehmetzahit <uyanikmehmetzahit@gmail.com>
2024-01-12 14:20:03 +0300

    controller update

--- WebAPI/Controllers/WorksController.cs
+++ WebAPI/Controllers/WorksController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Business.Abstract;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace WebAPI.Controllers
@@ -7,5 +9,69 @@ namespace WebAPI.Controllers
     [ApiController]
     public class WorksController : ControllerBase
     {
+        IWorkService _workService;
+
+        public WorksController(IWorkService workService)
+        {
+            _workService = workService;
+        }
+
+        [HttpGet("getall")]
+        public IActionResult GetAll()
+        {
+            var result = _workService.GetAll();
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpGet("getbyid")]
+        public IActionResult GetById(int roomId)
+        {
+            var result = _workService.GetById(roomId);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpPost("add")]
+        public IActionResult Add(Work work)
+        {
+            var result = _workService.Add(work);
+
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpPut("update")]
+
+        public IActionResult Update(Work work)
+        {
+            var result = _workService.Update(work);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpDelete("remove")]
+
+        public IActionResult Remove(Work work)
+        {
+            var result = _workService.Delete(work);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
     }
 }
